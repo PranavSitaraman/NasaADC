26b82a14a7c7eb834850685264cdddd6792edfdf
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2022-12-10 22:38:15 -0500

    improved pathfinding

--- scripts/pathfinding.cpp
+++ scripts/pathfinding.cpp
@@ -1,104 +1,124 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define INF 1e7
-
-const int V = 4000;
-
+#define ll long long
+#define ld long double
+const ll V = 4;
 int main()
 {
-    double x1, y1, x2, y2;
-    bool distanceOptimized = true;      // set false for elevation optimized
-
-    FILE* grid = fopen("../data/grid.txt", "r");
-    double** graph = (double**)malloc(sizeof(double*) * V);
-    for (int i = 0; i < V; i++) {
-        graph[i] = (double*)malloc(sizeof(double) * V);
-        for (int j = 0; j < V; j++) {
-            fscanf(grid, "%lf", &graph[i][j]);
+    ld x1 = 0, y1 = 0, x2 = 2, y2 = 3;  // start and end points from user selection (0-indexed)
+    bool distanceOptimized = true; // true for distance, false for elevation (from user selection)
+    FILE* input = fopen("../data/grid.txt", "r");
+    ld** graph = (ld**)malloc(sizeof(ld*) * V);
+    for (ll i = 0; i < V; i++)
+    {
+        graph[i] = (ld*)malloc(sizeof(ld) * V);
+        for (ll j = 0; j < V; j++) fscanf(input, "%Lf", &graph[i][j]);
+    }
+    fclose(input);
+    ld** elevation = (ld**)malloc(sizeof(ld*) * V * V);
+    for (ll i = 0; i < V * V; i++)
+    {
+        elevation[i] = (ld*)malloc(sizeof(ld) * V * V);
+        for (ll j = 0; j < V * V; j++) elevation[i][j] = INF;
+    }
+    if (distanceOptimized == false)
+    {
+        for (ll i = 0; i < V - 1; i++)
+        {
+            for (ll j = 0; j < V - 1; j++)
+            {
+                elevation[V * i + j][V * i + j] = 0;
+                elevation[V * i + j][V * i + j + 1] = elevation[V * i + j + 1][V * i + j] = (abs(graph[i][j + 1] - graph[i][j]) < 1.34) ? abs(graph[i][j + 1] - graph[i][j]) : INF;
+                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (abs(graph[i][j] - graph[i + 1][j]) < 1.34) ? abs(graph[i][j] - graph[i + 1][j]) : INF;
+                elevation[V * i + j][V * (i + 1) + j + 1] = elevation[V * (i + 1) + j + 1][V * i + j] = (abs(graph[i + 1][j + 1] - graph[i][j]) < 1.90) ? abs(graph[i + 1][j + 1] - graph[i][j]) : INF;
+            }
+        }
+        for (ll i = 0; i < V - 1; i++)
+        {
+            for (ll j = 1; j < V; j++)
+            {
+                elevation[V * i + j][V * i + j] = 0;
+                elevation[V * i + j][V * i + j - 1] = elevation[V * i + j - 1][V * i + j] = (abs(graph[i][j - 1] - graph[i][j]) < 1.34) ? abs(graph[i][j - 1] - graph[i][j]) : INF;
+                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (abs(graph[i][j] - graph[i + 1][j]) < 1.34) ? abs(graph[i][j] - graph[i + 1][j]) : INF;
+                elevation[V * i + j][V * (i + 1) + j - 1] = elevation[V * (i + 1) + j - 1][V * i + j] = (abs(graph[i + 1][j - 1] - graph[i][j]) < 1.90) ?abs(graph[i + 1][j - 1] - graph[i][j]) : INF;
+            }
         }
     }
-
-    double** elevation = (double**)malloc(sizeof(double*) * V * V);
-    for (int i = 0; i < V * V; i++) {
-        elevation[i] = (double*)malloc(sizeof(double) * V * V);
-        for (int j = 0; j < V * V; j++) {
-            elevation[i][j] = INF;
+    else
+    {
+        for (ll i = 0; i < V - 1; i++)
+        {
+            for (ll j = 0; j < V - 1; j++)
+            {
+                elevation[V * i + j][V * i + j] = 0;
+                elevation[V * i + j][V * i + j + 1] = elevation[V * i + j + 1][V * i + j] = (abs(graph[i][j + 1] - graph[i][j]) < 1.34) ? pow(pow(abs(graph[i][j + 1] - graph[i][j]), 2) + 25, 0.5) : INF;
+                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (abs(graph[i][j] - graph[i + 1][j]) < 1.34) ? pow(pow(abs(graph[i][j] - graph[i + 1][j]), 2) + 25, 0.5) : INF;
+                elevation[V * i + j][V * (i + 1) + j + 1] = elevation[V * (i + 1) + j + 1][V * i + j] = (abs(graph[i + 1][j + 1] - graph[i][j]) < 1.90) ? pow(pow(abs(graph[i + 1][j + 1] - graph[i][j]), 2) + 50, 0.5) : INF;
+            }
+        }
+        for (ll i = 0; i < V - 1; i++)
+        {
+            for (ll j = 1; j < V; j++)
+            {
+                elevation[V * i + j][V * i + j] = 0;
+                elevation[V * i + j][V * i + j - 1] = elevation[V * i + j - 1][V * i + j] = (abs(graph[i][j - 1] - graph[i][j]) < 1.34) ? pow(pow(abs(graph[i][j - 1] - graph[i][j]), 2) + 25, 0.5) : INF;
+                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (abs(graph[i][j] - graph[i + 1][j]) < 1.34) ? pow(pow(abs(graph[i][j] - graph[i + 1][j]), 2) + 25, 0.5) : INF;
+                elevation[V * i + j][V * (i + 1) + j - 1] = elevation[V * (i + 1) + j - 1][V * i + j] = (abs(graph[i + 1][j - 1] - graph[i][j]) < 1.90) ? pow(pow(abs(graph[i + 1][j - 1] - graph[i][j]), 2) + 50, 0.5) : INF;
+            }
         }
     }
-
-    for (int i = 0; i < V - 1; i++) {
-        for (int j = 0; j < V - 1; j++) {
-            elevation[V * i + j][V * i + j] = 0;
-            elevation[V * i + j][V * i + j + 1] = abs(graph[i][j + 1] - graph[i][j]);
-            elevation[V * i + j + 1][V * i + j] = abs(graph[i][j] - graph[i][j + 1]);
-            elevation[V * (i + 1) + j][V * i + j] = abs(graph[i][j] - graph[i + 1][j]);
-            elevation[V * i + j][V * (i + 1) + j] = abs(graph[i + 1][j] - graph[i][j]);
-            elevation[V * i + j][V * (i + 1) + j + 1] = abs(graph[i + 1][j + 1] - graph[i][j]);
-            elevation[V * (i + 1) + j + 1][V * i + j] = abs(graph[i][j] - graph[i + 1][j + 1]);
-        }
+    ld** dis = (ld**)malloc(sizeof(ld*) * V * V);
+    for (ll i = 0; i < V * V; i++)
+    {
+        dis[i] = (ld*)malloc(sizeof(ld) * V * V);
+        for (ll j = 0; j < V * V; j++) dis[i][j] = 0;
     }
-    for (int i = 0; i < V - 1; i++) {
-        for (int j = 1; j < V; j++) {
-            elevation[V * i + j][V * i + j] = 0;
-            elevation[V * i + j][V * i + j - 1] = abs(graph[i][j - 1] - graph[i][j]);
-            elevation[V * i + j - 1][V * i + j] = abs(graph[i][j] - graph[i][j - 1]);
-            elevation[V * (i + 1) + j][V * i + j] = abs(graph[i][j] - graph[i + 1][j]);
-            elevation[V * i + j][V * (i + 1) + j] = abs(graph[i + 1][j] - graph[i][j]);
-            elevation[V * i + j][V * (i + 1) + j - 1] = abs(graph[i + 1][j - 1] - graph[i][j]);
-            elevation[V * (i + 1) + j - 1][V * i + j] = abs(graph[i][j] - graph[i + 1][j - 1]);
-        }
+    ll** Next = (ll**)malloc(sizeof(ll*) * V * V);
+    for (ll i = 0; i < V * V; i++)
+    {
+        Next[i] = (ll*)malloc(sizeof(ll) * V * V);
+        for (ll j = 0; j < V * V; j++) Next[i][j] = -1;
     }
-
-    double** dis = (double**)malloc(sizeof(double*) * V * V);
-    for (int i = 0; i < V * V; i++) {
-        dis[i] = (double*)malloc(sizeof(double) * V * V);
-        for (int j = 0; j < V * V; j++) {
-            dis[i][j] = 0;
-        }
-    }
-
-    int** Next = (int**)malloc(sizeof(int*) * V * V);
-    for (int i = 0; i < V * V; i++) {
-        Next[i] = (int*)malloc(sizeof(int) * V * V);
-        for (int j = 0; j < V * V; j++) {
-            Next[i][j] = -1;
-        }
-    }
-
-    for (int i = 0; i < V * V; i++) {
-        for (int j = 0; j < V * V; j++) {
+    for (ll i = 0; i < V * V; i++)
+    {
+        for (ll j = 0; j < V * V; j++)
+        {
             dis[i][j] = elevation[i][j];
-            if (elevation[i][j] != INF)
-                Next[i][j] = j;
+            if (elevation[i][j] != INF) Next[i][j] = j;
         }
     }
-
-    for (int k = 0; k < V * V; k++) {
-        for (int i = 0; i < V * V; i++) {
-            for (int j = 0; j < V * V; j++) {
-                if (dis[i][k] == INF || dis[k][j] == INF)
-                    continue;
-                if (dis[i][j] > dis[i][k] + dis[k][j]) {
+    for (ll k = 0; k < V * V; k++)
+    {
+        for (ll i = 0; i < V * V; i++)
+        {
+            for (ll j = 0; j < V * V; j++)
+            {
+                if (dis[i][k] == INF || dis[k][j] == INF) continue;
+                if (dis[i][j] > dis[i][k] + dis[k][j])
+                {
                     dis[i][j] = dis[i][k] + dis[k][j];
                     Next[i][j] = Next[i][k];
                 }
             }
         }
     }
-
-    int u = x1 * V + y2;
-    int v = x2 * V + y2;
-    vector<int> path;
-    if (Next[u][v] != -1) {
-        path = { u };
-        while (u != v) {
+    ll u = x1 * V + y2;
+    ll v = x2 * V + y2;
+    FILE* output = fopen("../data/path.txt", "w");
+    if (Next[u][v] != -1)
+    {
+        fprintf(output, "%Ld %Ld %Lf\n", u / V, u % V, graph[u / V][u % V]);
+        while (u != v)
+        {
             u = Next[u][v];
-            path.push_back(u);
+            fprintf(output, "%Ld %Ld %Lf\n", u / V, u % V, graph[u / V][u % V]);
         }
-    } else
-        path = {};
-    for (int i = 0; i < path.size(); i++) {
-        cout << path[i] / V << " " << path[i] % V << " " << graph[path[i] / V][path[i] % V] << endl;
     }
+    else
+    {
+        return -1; // tell user "no! these settings will not work!"
+    }
+    fclose(output);
     return 0;
 }

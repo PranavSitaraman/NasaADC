8afae39f27065ef5db9a9b5e15fc113800bbbeed
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2022-12-13 20:29:04 -0500

    update pathcalculating and pathfinding

--- scripts/pathfinding.cpp
+++ scripts/pathfinding.cpp
@@ -4,132 +4,55 @@ using namespace std;
 #define ll int
 #define ld double 
 const ll dimension = 4000;
-const ll V = 100;
+const ll V = 50;
 int main()
 {
-    ld x1 = 3481 * V/dimension, y1 = 3687 * V/dimension, x2 = 2709 * V/dimension, y2 = 589 * V/dimension;  // start and end points from user selection (0-indexed)
-    bool distanceOptimized = true; // true for distance, false for elevation (from user selection)
-    ifstream input("../data/grid.txt");
+    ll x1 = 9, y1 = 9, x2 = 7, y2 = 1;
+    bool distanceOptimized = true;
+    x1 *= 400 * ((ld)V)/dimension;
+    y1 *= 400 * ((ld)V)/dimension;
+    x2 *= 400 * ((ld)V)/dimension;
+    y2 *= 400 * ((ld)V)/dimension;
+    ifstream inputgrid("../data/grid.txt");
     ld** graph = (ld**)malloc(sizeof(ld*) * V);
     for (ll i = 0; i < V; i++)
     {
-        for (ll k = 0; k < dimension/V - 1; k++) input.ignore(100000000L,'\n');
+        for (ll k = 0; k < dimension/V - 1; k++) inputgrid.ignore(100000000L,'\n');
         graph[i] = (ld*)malloc(sizeof(ld) * V);
         for (ll j = 0; j < V; j++)
         {
             ld a;
-            for (ll k = 0; k < dimension/V - 1; k++) input >> a;
-            input >> graph[i][j];
+            for (ll k = 0; k < dimension/V - 1; k++) inputgrid >> a;
+            inputgrid >> graph[i][j];
         }
     }
-    input.close();
-    ifstream slopeinput("../data/slopes.csv");
-    ld** slopes = (ld**)malloc(sizeof(ld*) * V);
-    for (ll i = 0; i < V; i++)
-    {
-        for (ll k = 0; k < dimension/V - 1; k++) slopeinput.ignore(100000000L,'\n');
-        slopes[i] = (ld*)malloc(sizeof(ld) * V);
-        for (ll j = 0; j < V; j++)
-        {
-            ld a;
-            for (ll k = 0; k < dimension/V - 1; k++) slopeinput >> a;
-            slopeinput >> slopes[i][j];
-        }
-    }
-    slopeinput.close();
+    inputgrid.close();
     ll** Next = (ll**)malloc(sizeof(ll*) * V * V);
-    for (ll i = 0; i < V * V; i++)
+    for (ll i = 0; i < V * V; i++) Next[i] = (ll*)malloc(sizeof(ll) * V * V);
+    if (distanceOptimized)
     {
-        Next[i] = (ll*)malloc(sizeof(ll) * V * V);
-        for (ll j = 0; j < V * V; j++) Next[i][j] = -1;
-    }
-    ld** elevation = (ld**)malloc(sizeof(ld*) * V * V);
-    for (ll i = 0; i < V * V; i++)
-    {
-        elevation[i] = (ld*)malloc(sizeof(ld) * V * V);
-        for (ll j = 0; j < V * V; j++) elevation[i][j] = INF;
-    }
-    if (distanceOptimized == false)
-    {
-        for (ll i = 0; i < V - 1; i++)
-        {
-            for (ll j = 0; j < V - 1; j++)
-            {
-                elevation[V * i + j][V * i + j] = 0;
-                elevation[V * i + j][V * i + j + 1] = elevation[V * i + j + 1][V * i + j] = (slopes[i][j+1] < 15) ? abs(graph[i][j + 1] - graph[i][j]) : INF;
-                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (slopes[i+1][j] < 15) ? abs(graph[i][j] - graph[i + 1][j]) : INF;
-                elevation[V * i + j][V * (i + 1) + j + 1] = elevation[V * (i + 1) + j + 1][V * i + j] = (slopes[i+1][j+1] < 15) ? abs(graph[i + 1][j + 1] - graph[i][j]) : INF;
-            }
-        }
-        for (ll i = 0; i < V - 1; i++)
-        {
-            for (ll j = 1; j < V; j++)
-            {
-                elevation[V * i + j][V * i + j] = 0;
-                elevation[V * i + j][V * i + j - 1] = elevation[V * i + j - 1][V * i + j] = (slopes[i][j-1] < 15) ? abs(graph[i][j - 1] - graph[i][j]) : INF;
-                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (slopes[i+1][j] < 15) ? abs(graph[i][j] - graph[i + 1][j]) : INF;
-                elevation[V * i + j][V * (i + 1) + j - 1] = elevation[V * (i + 1) + j - 1][V * i + j] = (slopes[i+1][j-1] < 15) ? abs(graph[i + 1][j - 1] - graph[i][j]) : INF;
-            }
-        }
+        ifstream input("../data/distancegrid.txt");
+        for (ll i = 0; i < V * V; i++) for (ll j = 0; j < V * V; j++) input >> Next[i][j];
+        input.close();
     }
     else
     {
-        for (ll i = 0; i < V - 1; i++)
-        {
-            for (ll j = 0; j < V - 1; j++)
-            {
-                elevation[V * i + j][V * i + j] = 0;
-                elevation[V * i + j][V * i + j + 1] = elevation[V * i + j + 1][V * i + j] = (slopes[i][j+1] < 15) ? pow(pow(abs(graph[i][j + 1] - graph[i][j]), 2) + 25, 0.5) : INF;
-                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (slopes[i+1][j] < 15) ? pow(pow(abs(graph[i][j] - graph[i + 1][j]), 2) + 25, 0.5) : INF;
-                elevation[V * i + j][V * (i + 1) + j + 1] = elevation[V * (i + 1) + j + 1][V * i + j] = (slopes[i+1][j+1] < 15) ? pow(pow(abs(graph[i + 1][j + 1] - graph[i][j]), 2) + 50, 0.5) : INF;
-            }
-        }
-        for (ll i = 0; i < V - 1; i++)
-        {
-            for (ll j = 1; j < V; j++)
-            {
-                elevation[V * i + j][V * i + j] = 0;
-                elevation[V * i + j][V * i + j - 1] = elevation[V * i + j - 1][V * i + j] = (slopes[i][j-1] < 15) ? pow(pow(abs(graph[i][j - 1] - graph[i][j]), 2) + 25, 0.5) : INF;
-                elevation[V * (i + 1) + j][V * i + j] = elevation[V * i + j][V * (i + 1) + j] = (slopes[i+1][j] < 15) ? pow(pow(abs(graph[i][j] - graph[i + 1][j]), 2) + 25, 0.5) : INF;
-                elevation[V * i + j][V * (i + 1) + j - 1] = elevation[V * (i + 1) + j - 1][V * i + j] = (slopes[i+1][j-1] < 15) ? pow(pow(abs(graph[i + 1][j - 1] - graph[i][j]), 2) + 50, 0.5) : INF;
-            }
-        }
+        ifstream input("../data/elevationgrid.txt");
+        for (ll i = 0; i < V * V; i++) for (ll j = 0; j < V * V; j++) input >> Next[i][j];
+        input.close();
     }
-    for (ll i = 0; i < V * V; i++)
-    {
-        for (ll j = 0; j < V * V; j++)
-        {
-            if (elevation[i][j] != INF) Next[i][j] = j;
-        }
-    }
-    for (ll k = 0; k < V * V; k++)
-    {
-        for (ll i = 0; i < V * V; i++)
-        {
-            for (ll j = 0; j < V * V; j++)
-            {
-                if (elevation[i][k] == INF || elevation[k][j] == INF) continue;
-                if (elevation[i][j] > elevation[i][k] + elevation[k][j])
-                {
-                    elevation[i][j] = elevation[i][k] + elevation[k][j];
-                    Next[i][j] = Next[i][k];
-                }
-            }
-        }
-    }
-    ll u = x1 * V + y2;
+    ll u = x1 * V + y1;
     ll v = x2 * V + y2;
     ofstream output("../data/path.txt");
     if (Next[u][v] != -1)
     {
-        output << u / V << " " << u % V << " " << graph[u / V][u % V] << endl;
+        output << (u / V) * dimension/V << " " << (u % V) * dimension/V << " " << graph[u / V][u % V] << endl;
         while (u != v)
         {
             u = Next[u][v];
             output << (u / V) * dimension/V << " " << (u % V) * dimension/V << " " << graph[u / V][u % V] << endl;
         }
     }
-    else return -1; // tell user "no! these settings will not work!"
     output.close();
     return 0;
 }
